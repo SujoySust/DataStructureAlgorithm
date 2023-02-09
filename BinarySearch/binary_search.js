4bb93bd50cbd5d2d52e950d7a11de5078c828ac1
Sujoy Nath <sujoy.csesust@gmail.com>
2023-02-09 07:40:57 +0600

    modified package json

--- BinarySearch/binary_search.js
+++ BinarySearch/binary_search.js
@@ -1,19 +1,27 @@
 function binarySearch(arr, target) {
-    var start = 0;
-    var end = Number(arr.length);
-    while (start <= end) {
-        var mid = start + Math.floor((end - start) / 2);
-        if (arr[mid] == target) {
-            return mid;
-        }
-        else if (arr[mid] > target) {
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
+  var start = 0;
+  var end = Number(arr.length) - 1;
+  const is_asc = arr[start] - arr[end];
+  while (start <= end) {
+    var mid = start + Math.floor((end - start) / 2);
+    if (arr[mid] == target) {
+      return mid;
     }
-    return -1;
+    if (is_asc) {
+      if (arr[mid] > target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    } else {
+      if (arr[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+  return -1;
 }
 var arr = [2, 4, 6, 8, 10];
 var target = 6;
