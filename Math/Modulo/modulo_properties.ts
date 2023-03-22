f6edd9823ac5b4c8b3f16d56bf646571de201985
Sujoy Nath <sujoy.csesust@gmail.com>
2023-03-22 12:03:06 +0600

    added modulo properties

---
+++ Math/Modulo/modulo_properties.ts
@@ -0,0 +1,11 @@
+/**
+ * Properties of modulo (%)
+ * (a+b) % m = (( a % m ) + ( b % m )) % m
+ * (a-b) % m = ( (a % m ) - ( b % m ) + m ) % m
+ * (a * b) % m = ( (a % m ) * ( b % m )) % m
+ * (a/b) % m = (( a % m ) * ( b^-1 % m )) % m
+ * ( a % m ) % m = a % m
+ * 
+ * If p is a prime number which is not a divisor of b the ab % p = a % p 
+ *  
+ */
--- Math/PrimeNumber/factors_of_a_number.ts
+++ Math/PrimeNumber/factors_of_a_number.ts
@@ -1,8 +1,10 @@
 /**
  * Factors of a number
  * All number that divisible is called factor of number
+ * 
  */
 
+// Solution O(n))
 function factorOfNumber(n: number) {
     for (let i = 1; i <=n; i++) {
         if (n %i == 0 ) {
@@ -10,5 +12,38 @@ function factorOfNumber(n: number) {
         }
     }
 }
+
+// Solution O(log(sqrt(n)))
+function factorOfNumber2(n:number) {
+    for (let i=1; i <=Math.sqrt(n); i++) {
+        if (n%i == 0) {
+            if (n%i == i) {
+                console.log(i);
+            } else {
+                console.log(n/i); 
+            }
+        }
+    }
+}
+
+
+// Solution O(log(sqrt(n)))
+function factorOfNumber3(n:number) {
+    let arr: Array<number> = [];
+    for (let i=1; i <=Math.sqrt(n); i++) {
+        if (n%i == 0) {
+            if (n%i == i) {
+                console.log(i);
+            } else {
+                arr.push(n/i);
+            }
+        }
+    }
+
+    for (let i=arr.length - 1;i>=0;i--) {
+        console.log(i);
+    }
+}
+
 
 factorOfNumber(10);
