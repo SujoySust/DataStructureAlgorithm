46912d2e8b528fb64a220d915ebd7fb2c638462b
Sujoy Nath <sujoy.csesust@gmail.com>
2024-02-07 10:10:53 +0600

    add remove duplicate problem

--- Array/ProblemSolving/remove_duplicate_from_sorted_array.ts
+++ Array/ProblemSolving/remove_duplicate_from_sorted_array.ts
@@ -1,5 +1,24 @@
-function helloWorld() {
-    return 'hello worl';
-}
+function removeDuplicatesBruteForce(nums: number[]): number {
+    const map: {[key: number]: number} = {};
+    const newArr: number[] = [];
+    for (let i = 0; i< nums.length; i++) {
+        if (!map[nums[i]]) {
+            newArr.push(nums[i]);
+            map[nums[i]] = 1;
+        }
+    }
+    return newArr.length;
+};
 
-const result = helloWorld(); 
+function removeDuplicatesOptimal(nums: number[]): number {
+    let i = 0;
+    for (let j = 1; j< nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
+
+console.log(removeDuplicatesBruteForce([1,1,1,1,2,2,3,3,3,4,5,5,5,6]))
---
+++ Array/ProblemSolving/rotate_array.ts
@@ -0,0 +1,34 @@
+function rotateArrayBruteForce(nums: number[], k: number): void {
+    const newArr: number[] = [];
+    let start = nums.length - k;
+    for (let i = start; i< nums.length; i++) {
+        newArr.push(nums[i]);
+    }
+    for (let i = 0; i < start; i++) {
+        newArr.push(nums[i]);
+    }
+    nums = newArr;
+    console.log(nums);
+}
+
+function rotateArrayOptimal(nums: number[], k: number): void {
+    const length = nums.length;
+    k = k % length;
+    nums = reverseArray(nums, 0, length - k - 1);
+    nums = reverseArray(nums, length - k, length - 1);
+    nums = reverseArray(nums, 0, length - 1);
+    console.log(nums);
+}
+
+function reverseArray(nums: number[], start = 0, end = nums.length) {
+    while (start < end) {
+        const temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start ++;
+        end --;
+    }
+    return nums;
+}
+
+console.log(rotateArrayOptimal([-1], 2));
